0f9754541891ba4b913fbbaa801811d64014bdcc
Andrey Zhovtukha <andreyzhov@gmail.com>
2022-12-14 17:53:57 +0200

    car commit

---
+++ src/main/java/CarColor.java
@@ -0,0 +1,14 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public enum CarColor {
+
+    @JsonProperty("red")
+    RED,
+
+    @JsonProperty("green")
+    GREEN,
+
+    @JsonProperty("blue")
+    BLUE
+
+}
