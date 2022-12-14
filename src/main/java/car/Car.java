3f1c5dfccd91dde279f9aaadbcab7ee0849c4a43
Andrey Zhovtukha <andreyzhov@gmail.com>
2022-12-14 18:00:41 +0200

    car commit

---
+++ src/main/java/car/Car.java
@@ -0,0 +1,52 @@
+package car;
+
+public class Car {
+    private String model;
+    private final String name;
+    private String carNumber;
+    private CarColor color;
+
+    public Car(String name, String carNumber) {
+        this.name = name;
+        this.carNumber = carNumber;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(CarColor color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setCarNumber(String carNumber) {
+        this.carNumber = carNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCarNumber() {
+        return carNumber;
+    }
+
+    public CarColor getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "car name='" + name + '\'' +
+                ", model=" + model +
+                ", car number=" + carNumber +
+                ", car color=" + color +
+                '}';
+    }
+}
+
